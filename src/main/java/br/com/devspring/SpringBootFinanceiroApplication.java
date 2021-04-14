169b357616abcae1a1e66f0dd1e0f2d4fdda7a86
Alison Rodrigues <alison.aro@gmail.com>
2021-04-14 16:59:18 -0300

    alterações na inicialização do sistema para instanciar Objetos FormaPagamento

--- src/main/java/br/com/devspring/SpringBootFinanceiroApplication.java
+++ src/main/java/br/com/devspring/SpringBootFinanceiroApplication.java
@@ -1,14 +1,31 @@
 package br.com.devspring;
 
+import br.com.devspring.domain.FormaPagamento;
+import br.com.devspring.repository.FormaPagamentoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class SpringBootFinanceiroApplication {
+public class SpringBootFinanceiroApplication implements CommandLineRunner { //inteface permite executar comandos na inicialização do sistema.
+
+    @Autowired
+    private FormaPagamentoRepository formaPagamentoDAO;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootFinanceiroApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        FormaPagamento formaPagamento1 = new FormaPagamento("Dinheiro");
+        FormaPagamento formaPagamento2 = new FormaPagamento("Cartão");
+
+        formaPagamentoDAO.saveAll(Arrays.asList(formaPagamento1,formaPagamento2));
+
+    }
 }
--- src/main/java/br/com/devspring/resources/FormaPagamentoResource.java
+++ src/main/java/br/com/devspring/resources/FormaPagamentoResource.java
@@ -28,11 +28,12 @@ public class FormaPagamentoResource {
     //@RequestMapping(method = RequestMethod.GET, path = "/{id}")
     //busca informação passando um id. Ex:localhost:8080/formapagamento/2
     @GetMapping(path = "/{id}")
-    public ResponseEntity<?> getFormaPagamentoById(@PathVariable("id") Long id) {
+    public ResponseEntity<?> getFormaPagamentoById(@PathVariable Long id) {
         Optional<FormaPagamento> formmaPagamento = formaPagamentoService.getPorID(id);
         if ((formmaPagamento.isEmpty()) || (formmaPagamento == null))
             return new ResponseEntity<>(new CustomErrorType("Forma Pagamento not found"), HttpStatus.NOT_FOUND); //retorna exceção personalizada
-        return new ResponseEntity<>(formmaPagamento, HttpStatus.OK); //retorna a forma de pagamento.*/
+        //return new ResponseEntity<>(formmaPagamento, HttpStatus.OK); //retorna a forma de pagamento.*/
+        return ResponseEntity.ok().body(formmaPagamento);
 
     }
 
@@ -53,7 +54,7 @@ public class FormaPagamentoResource {
 
     @DeleteMapping(path = "/{id}")
     //@RequestMapping(method = RequestMethod.DELETE) //deleta informação. Ex:localhost:8080/formapagamento ; passar no body o Jason
-    public ResponseEntity<?> delete(@PathVariable("id") Long id) {
+    public ResponseEntity<?> delete(@PathVariable Long id) {
         formaPagamentoService.deletar(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
