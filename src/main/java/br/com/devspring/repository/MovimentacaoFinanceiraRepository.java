8011f5312b19739bfc7b8fad0ee5532be90d787e
Alison Rodrigues <alison.aro@gmail.com>
2021-04-15 16:38:45 -0300

    Adição de método para tratar os demais erros que podem ocorrer na classe Handler

--- src/main/java/br/com/devspring/repository/MovimentacaoFinanceiraRepository.java
+++ src/main/java/br/com/devspring/repository/MovimentacaoFinanceiraRepository.java
@@ -3,8 +3,6 @@ package br.com.devspring.repository;
 import br.com.devspring.domain.MovimentacaoFinanceira;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-
 public interface MovimentacaoFinanceiraRepository extends CrudRepository<MovimentacaoFinanceira,Long> {
 
 }
--- src/main/java/br/com/devspring/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/br/com/devspring/resources/exception/ResourceExceptionHandler.java
@@ -1,12 +1,15 @@
 package br.com.devspring.resources.exception;
 
 import br.com.devspring.services.exception.ObjectNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
@@ -14,25 +17,34 @@ import java.util.stream.Collectors;
 
 //Manipulador de exceções do Resource(recursos)
 @ControllerAdvice
-public class ResourceExceptionHandler {
+public class ResourceExceptionHandler extends ResponseEntityExceptionHandler { //A classe ResponseEntityExceptionHandler trata a granfe maioria das exception
 
     //Asinatura padrão do @ControllerAdvice
     @ExceptionHandler(ObjectNotFoundException.class) //Indica que esse método trata esse tipo de exception.
-    public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+    public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException ex, HttpServletRequest request){
 
-        StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(),e.getMessage(), System.currentTimeMillis(), e.getClass().getName());
+        StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(),ex.getMessage(), System.currentTimeMillis(), ex.getClass().getName());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
     }
-    //Asinatura padrão do @ControllerAdvice
-    @ExceptionHandler(MethodArgumentNotValidException.class) //Indica que esse método trata esse tipo de exception.
-    public ResponseEntity<ValidationError> argumentNotFound(MethodArgumentNotValidException e, HttpServletRequest request){
-        List<FieldError> fieldErrors = e.getBindingResult().getFieldErrors(); //Recupera os campos que tiveram falha na validação
+
+    //@ExceptionHandler(MethodArgumentNotValidException.class) //Indica que esse método trata esse tipo de exception.
+    //public ResponseEntity<ValidationError> argumentNotFound(MethodArgumentNotValidException e, HttpServletRequest request){
+    @Override
+    public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors(); //Recupera os campos que tiveram falha na validação
         String fields = fieldErrors.stream().map(FieldError::getField).collect(Collectors.joining(","));
         String fieldMessages = fieldErrors.stream().map(FieldError::getDefaultMessage).collect(Collectors.joining(","));
 
-        ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(),"Fields Validation Error", System.currentTimeMillis(), e.getClass().getName(),fields, fieldMessages);
+        ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(),"Fields Validation Error", System.currentTimeMillis(), ex.getClass().getName(),fields, fieldMessages);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 
+    //ERRO que trata as demais exception
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        StandardError err = new StandardError(status.value(),ex.getMessage(), System.currentTimeMillis(), ex.getClass().getName());
+        return ResponseEntity.status(status.value()).body(err);
+
+    }
 
 }
